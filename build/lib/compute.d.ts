52a8be0576ea8b2fc1da1bfbb9a0f74691f254c4
helix.d <helixd.2k18@gmail.com>
2020-02-04 16:09:02 +0800

    fix TypeScript definition

--- build/lib/compute.d.ts
+++ build/lib/compute.d.ts
@@ -1,32 +1,32 @@
 declare namespace Compute {
-    declare interface Workgroup{}
-    declare interface Worktask{}
-    declare interface TaskInterface {
-        name?: string = "main";
-        args?: number[] = []; // TODO: correct typing
-        resp?: number = 0;
-        workgroup?: Workgroup;
+    interface Workgroup{}
+    interface Worktask{}
+    interface TaskInterface {
+        name?: string
+        args?: number[] // TODO: correct typing
+        resp?: number
+        workgroup?: Workgroup
     }
 
-    declare class Workgroup {
+    class Workgroup {
         constructor();
         initiate(module:string,threads?:number,supportCode?:string) : Promise<Workgroup>;
         wait(resp: Promise<any>) : Promise<Workgroup>;
         task(input?:TaskInterface) : Worktask;
         allocate(size?:number) : number;
         free(pointer:number) : any;
-        map<T=Uint8Array>(ptr:number,range?:number,type?:new(arrayBuffer?:ArrayBuffer,offset?:number,range?:number)=>T = Uint8Array) : T;
+        map<T=Uint8Array>(ptr:number,range?:number,type?:new(arrayBuffer?:ArrayBuffer,offset?:number,range?:number)=>T) : T;
     }
 
-    declare class Worktask {
+    class Worktask {
         constructor(input?:TaskInterface);
         dispatch(): Promise<any>; // TODO: fix result type
         support (): Promise<any>; // TODO: fix result type
         get id(): number;
     }
 
-    declare interface Module {
+    interface Module {
         init() : Promise<any>;
-        workgroup(module:string,threads?:number =1,supportCode?:string=``) : Promise<Workgroup>;
+        workgroup(module:string,threads?:number,supportCode?:string) : Promise<Workgroup>;
     }
 }
